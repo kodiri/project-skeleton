a4903760e33e7a679b803f5f369f7dfae5f729d6
administrator <admin@administrators-MacBook-Air.local>
2020-02-04 16:02:19 +0000

    implement basic schemas for yoga and prisma

---
+++ src/backend/createServer.js
@@ -0,0 +1,21 @@
+const { GraphQLServer } = require('graphql-yoga');
+const Mutation = require('./resolvers/Mutation');
+const Query = require('./resolvers/Query');
+const db = require('./db');
+
+//Create the GraphQL yoga server
+function createServer() {
+    return new GraphQLServer({
+        typeDefs: 'src/backend/schema.graphql',
+        resolvers: {
+            Mutation,
+            Query,
+        },
+        resolverValidationOptions: {
+            requireResolversForResolveType: false,
+        },
+        context: req => ({ ...req, db }),
+    });
+}
+
+module.exports = createServer;
---
+++ src/backend/resolvers/Mutation.js
@@ -0,0 +1,13 @@
+const Mutations = {
+    async createUser(parent, args, ctx, info) {
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args
+            }
+        }, info);
+        return user;
+    },
+}
+
+
+module.exports = Mutations;

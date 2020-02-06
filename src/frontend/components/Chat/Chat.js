5196fdb248ad36e1a05fb46528aeb96b98161279
r norman <rochellenorman0@gmail.com>
2020-02-06 14:21:29 +0000

    updated the join component

--- src/frontend/components/Chat/Chat.js
+++ src/frontend/components/Chat/Chat.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Chat = () => {
+
     return (
         <h1>Chat</h1>
     )
--- src/frontend/components/Join/Join.js
+++ src/frontend/components/Join/Join.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Join = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
     return (
-        <h1>Join</h1>
+     <div className ="joinOuterContainer" >
+     <div className="joinInnerContainer">
+     <h1 className="heading">Join</h1>
+     <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)}/></div>
+     <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setName(event.target.value)}/></div>
+     <Link>
+<button className = "button mt-20" type="submit">Sign In</button>
+     </Link>
+     </div>
+     
+     </div>
+
     )
 }
 

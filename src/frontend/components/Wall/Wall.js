d30f8c5342cadfe7cea9c7d0d915e6a6a68676c5
oliviachae <lovelychae91@gmail.com>
2020-02-07 14:56:01 +0000

    Create Input box and style it

---
+++ src/frontend/components/Wall/Wall.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Wall.css'
+
+export default class Wall extends React.Component {
+    render() {
+        return(
+            <div className='Wall'>
+            <header>
+                <form className='userWall'>
+                    <input type='text' placeholder='Enter Text' />
+                    <button 
+                    style={{backgroundColor:'rgb(202,156,24)', height: '50px', width: '80px', color: 'white'}} 
+                    type='submit'>Add</button>
+                </form>
+            </header>
+            </div>
+        );
+    }
+}

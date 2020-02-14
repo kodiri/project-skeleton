cdf7699311ce6b585baa1dc5f5000428df29f4fd
administrator <admin@administrators-MacBook-Air.local>
2020-02-14 14:41:12 +0000

    commented hardcoded server port

--- backend/src/index.js
+++ backend/src/index.js
@@ -45,7 +45,7 @@ server.express.use(async (req, res, next) => {
 });
 
 server.start({
-    port: process.env.NODE_PORT,
+    // port: process.env.NODE_PORT,
     cors: {
         credentials: true,
         origin: process.env.FRONTEND_URL

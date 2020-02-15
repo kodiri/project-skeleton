fc5dd907891a0675ee0e5da1fac25339da6135ab
administrator <admin@administrators-MacBook-Air.local>
2020-02-15 19:38:14 +0000

    solve hidden conflicts

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ const db = require('./db');
 const createServer = require('./createServer');
 const server = createServer();
 
+
 server.express.use(cookieParser());
 
 server.express.use((req, res, next) => {
@@ -29,8 +30,7 @@ server.express.use(async (req, res, next) => {
 server.start({
     cors: {
         credentials: true,
-        origin: true,
-        // origin: process.env.FRONTEND_URL
+        origin: process.env.FRONTEND_URL
     }
 }, deets => {
     console.log(`Server is now running on port http://localhost:${deets.port}`)

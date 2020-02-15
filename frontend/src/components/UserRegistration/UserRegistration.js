94c7f0c2ebf3e304f5be3f09654fb0c5375c00f8
administrator <admin@administrators-MacBook-Air.local>
2020-02-15 17:24:04 +0000

    change frontend endpoint

--- frontend/src/components/UserRegistration/UserRegistration.js
+++ frontend/src/components/UserRegistration/UserRegistration.js
@@ -21,7 +21,7 @@ const SIGNUP_MUTATION = gql`
             id
             email
             name
-        }
+        } 
     }
 `;
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
 export const endpoint = `http://localhost:4000`;
-export const prodEndpoint = `https://project-skeleton-ef0d74e0b6.herokuapp.com/skeleton-prod/prod`;
+export const prodEndpoint = `https://project-skeleton-prod.herokuapp.com/`;

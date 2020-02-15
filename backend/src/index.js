0d2dfb13ccd1fa38bead35be37f333e1f3480f4f
administrator <admin@administrators-MacBook-Air.local>
2020-02-15 17:49:58 +0000

    open cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,8 @@ server.express.use(async (req, res, next) => {
 server.start({
     cors: {
         credentials: true,
-        origin: [process.env.FRONTEND_URL, "http://localhost:3000/"]
+        origin: true,
+        // origin: process.env.FRONTEND_URL
     }
 }, deets => {
     console.log(`Server is now running on port http://localhost:${deets.port}`)

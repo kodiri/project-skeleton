427916e2810b627d6c083d544af4341af23280ec
administrator <admin@administrators-MacBook-Air.local>
2020-02-15 18:11:25 +0000

    samesite cookie to lax

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -18,6 +18,7 @@ const Mutations = {
         ctx.response.cookie('token', token, {
             httpOnly: true,
             maxAge: 1000 * 60 * 60 * 24 * 365,
+            SameSite: "Lax"
         })
         return user;
     },

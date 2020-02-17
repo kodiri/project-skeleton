9404c418b5ab91116becc6b924a9e1983f757480
administrator <admin@administrators-MacBook-Air.local>
2020-02-17 13:08:50 +0000

    another try with where

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -6,9 +6,17 @@ const Query = {
         const userId = await ctx.request.userId;
         return ctx.db.query.user({ where: { id: userId } }, info);
     },
-    async getUser(parent, args, ctx, info) {
-        const name = await ctx.request.name;
-        return ctx.db.query.user({ where: { name: name } }, info);
+    async user(parent, args, ctx, info) {
+        if (!ctx.request.userId) {
+            // throw new Error('You arent logged in!');
+        }
+        const user = await ctx.db.query.users(
+            {
+                where: { name: args.name },
+            },
+            info
+        );
+        return user;
     },
 };
 

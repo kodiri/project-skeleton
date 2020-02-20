db705af2720d4b3e8f8c0a572e59e2834e6f7bbf
lexjames06 <57917545+lexjames06@users.noreply.github.com>
2020-02-20 16:14:11 +0000

    Add styling to the homeboy component for the homepage (#32)
    * Adjust homebody styling

--- frontend/src/components/HomeBody.js
+++ frontend/src/components/HomeBody.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
     BrowserRouter as Router,
     Link,
 } from "react-router-dom";
+import skull from '../assets/icons/skull.png'
+import './styles/HomeBody.css'
 
-class HomeBody extends Component {
+class HomeBody extends React.Component {
     render() {
         return (
-            <div>
-                <div className='welcomeMessage'>
-                    <h1>Welcome...</h1>
+            <div className='homebody-outer-container'>
+                <div className='homebody-inner-container'>
+                    <div className='skull-logo'>
+                        <img src={skull} alt='skull logo' />
+                    </div>
+                    <div className='welcomeMessage'>
+                        <h1>Welcome...</h1>
+                        <h4>Some secondary message to go here...</h4>
+                    </div>
+                    <div className='homebody-buttons'>
+                        <Link to="/login">
+                            <button className='homebody-button' id='login'><h3>Log In</h3></button>
+                        </Link>
+                        <Link to="/register">
+                            <button className='homebody-button' id='register'><h3>Sign Up</h3></button>
+                        </Link>
+                    </div>
+                    <div className='homeBodyText'>
+                        <h3>What we do</h3>
+                        <h3>How it works</h3>
+                        <h3>Why join</h3>
+                    </div>
                 </div>
-                <Router>
-                    <Link to="/login">
-                        <button><h3>Log In</h3></button>
-                    </Link>
-                    <Link to="/register">
-                        <button><h3>Sign Up</h3></button>
-                    </Link>
-                </Router>
-                <div className='homeBodyText'>
-                    <h3>What we do</h3>
-                    <h3>How it works</h3>
-                    <h3>Why join</h3>
+                <div className='homebody-footer-links'>
+                    <h5>Terms and Conditions</h5>
+                    <h5>Privacy and Cookies</h5>
+                    <h5>FAQs</h5>
                 </div>
             </div>
         );

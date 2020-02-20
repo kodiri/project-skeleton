bc62929bb9b3b5a3bd8c6005c850d054253209b1
Lex James <Phlex06@Alice-C.localdomain>
2020-02-20 15:59:51 +0000

    Adjust homebody styling

--- frontend/src/components/HomeBody.js
+++ frontend/src/components/HomeBody.js
@@ -3,6 +3,7 @@ import {
     BrowserRouter as Router,
     Link,
 } from "react-router-dom";
+import skull from '../assets/icons/skull.png'
 import './styles/HomeBody.css'
 
 class HomeBody extends React.Component {
@@ -10,22 +11,31 @@ class HomeBody extends React.Component {
         return (
             <div className='homebody-outer-container'>
                 <div className='homebody-inner-container'>
-                <div className='welcomeMessage'>
-                    <h1>Welcome...</h1>
+                    <div className='skull-logo'>
+                        <img src={skull} alt='skull logo' />
+                    </div>
+                    <div className='welcomeMessage'>
+                        <h1>Welcome...</h1>
+                        <h4>Some secondary message to go here...</h4>
+                    </div>
+                    <div className='homebody-buttons'>
+                        <Link to="/login">
+                            <button className='homebody-button' id='login'><h3>Log In</h3></button>
+                        </Link>
+                        <Link to="/register">
+                            <button className='homebody-button' id='register'><h3>Sign Up</h3></button>
+                        </Link>
+                    </div>
+                    <div className='homeBodyText'>
+                        <h3>What we do</h3>
+                        <h3>How it works</h3>
+                        <h3>Why join</h3>
+                    </div>
                 </div>
-                <div className='homebody-buttons'>
-                    <Link to="/login">
-                        <button className='homebody-button' id='login'><h3>Log In</h3></button>
-                    </Link>
-                    <Link to="/register">
-                        <button className='homebody-button' id='register'><h3>Sign Up</h3></button>
-                    </Link>
-                </div>
-                <div className='homeBodyText'>
-                    <h3>What we do</h3>
-                    <h3>How it works</h3>
-                    <h3>Why join</h3>
-                </div>
+                <div className='homebody-footer-links'>
+                    <h5>Terms and Conditions</h5>
+                    <h5>Privacy and Cookies</h5>
+                    <h5>FAQs</h5>
                 </div>
             </div>
         );

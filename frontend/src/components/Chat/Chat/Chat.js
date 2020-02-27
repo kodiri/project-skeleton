c5c1f78c8ba0bc29f54d995324b9e51d821e7da7
administrator <admin@administrators-MacBook-Air.local>
2020-02-27 17:49:30 +0000

    fdsd

--- frontend/src/components/Chat/Chat/Chat.js
+++ frontend/src/components/Chat/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'https://skeleton-project-chat.herokuapp.com/chat/';
+  const ENDPOINT = 'https://skeleton-project-chat.herokuapp.com/';
   // const ENDPOINT = 'https://bravo-chat.herokuapp.com/';
   // const ENDPOINT = 'http://localhost:5000/';
 
